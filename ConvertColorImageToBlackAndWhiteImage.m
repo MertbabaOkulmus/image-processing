dde514dcbaa710cce46fa6016ebe502a5f868e36
Mertbaba Okulmuş <51191427+MertbabaOkulmus@users.noreply.github.com>
2020-11-21 19:40:04 +0300

    Güncelleme

--- ConvertColorImageToBlackAndWhiteImage.m
+++ ConvertColorImageToBlackAndWhiteImage.m
@@ -1,10 +1,11 @@
 clc; clear all; close all;
 
+
 img=imread('peppers.png');
 figure,imshow(img);
 
 figure,histogram(img);
 
-ikilik=binary(img,125);%görüntünü arka plandan daha net ayrýla bilmesi için, görüntünün eþik deðerini histoðramdan belirlememiz gerekir
+ikilik=binary(img,125);%gï¿½rï¿½ntï¿½nï¿½ arka plandan daha net ayrï¿½la bilmesi iï¿½in, gï¿½rï¿½ntï¿½nï¿½n eï¿½ik deï¿½erini histoï¿½ramdan belirlememiz gerekir
 
 figure,imshow(ikilik);
